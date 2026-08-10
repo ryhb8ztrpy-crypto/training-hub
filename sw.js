ba1425b13dfd77cf3c31b42f954b513b3e7e5ba6
Stas <stasmkh06@gmail.com>
2026-08-10 17:11:27 +0300

    HRV red under-recovery zone + official marathon site link

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Training Hub service worker
 // CACHE_VERSION bump = принудительное обновление у всех клиентов
-const CACHE_VERSION = 'th-v2026-08-10i';
+const CACHE_VERSION = 'th-v2026-08-10k';
 const ASSETS = [
   './',
   './index.html',
