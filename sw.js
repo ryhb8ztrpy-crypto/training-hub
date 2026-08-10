12d86127cc9d0d980aa59e988889667652a954a6
Stas <stasmkh06@gmail.com>
2026-08-10 16:27:02 +0300

    Progress bars to half marathon and marathon

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Training Hub service worker
 // CACHE_VERSION bump = принудительное обновление у всех клиентов
-const CACHE_VERSION = 'th-v2026-08-10d';
+const CACHE_VERSION = 'th-v2026-08-10e';
 const ASSETS = [
   './',
   './index.html',
