1855e54536d406b2e3d9cb2a95e75cfd781349c0
Stas <stasmkh06@gmail.com>
2026-08-16 08:58:25 +0300

    Week 10-16.08 closed: facts from Garmin, long run moved to Sat, new predictions

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Training Hub service worker
 // CACHE_VERSION bump = принудительное обновление у всех клиентов
-const CACHE_VERSION = 'th-v2026-08-10k';
+const CACHE_VERSION = 'th-v2026-08-16';
 const ASSETS = [
   './',
   './index.html',
