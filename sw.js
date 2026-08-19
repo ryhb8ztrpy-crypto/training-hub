6f7c5388f92536aed73c9e0c9f7bbe9d0838c7cb
Stas <stasmkh06@gmail.com>
2026-08-19 06:07:21 +0000

    Week 17-23.08: lower load after move & sleep loss (edit 18.08)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Training Hub service worker
 // CACHE_VERSION bump = принудительное обновление у всех клиентов
-const CACHE_VERSION = 'th-v2026-08-16';
+const CACHE_VERSION = 'th-v2026-08-19';
 const ASSETS = [
   './',
   './index.html',
