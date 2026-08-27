4207e5d71846efbe8bdcc1d528166659126d2e44
Stas <stasmkh06@gmail.com>
2026-08-27 17:53:26 +0300

    Update 27.08: facts and taper week

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Training Hub service worker
 // CACHE_VERSION bump = принудительное обновление у всех клиентов
-const CACHE_VERSION = 'th-v2026-08-19';
+const CACHE_VERSION = 'th-v2026-08-27';
 const ASSETS = [
   './',
   './index.html',
